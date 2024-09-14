1cb8ac4ce36b3a31934208b4e8dd3b8ffcf02fdf
MarcusWentz <markzcollege404@gmail.com>
2024-09-14 17:18:09 -0400

    Tested string

--- contractsRequestTypes/testRequestString.sol
+++ contractsRequestTypes/testRequestString.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.26;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
@@ -8,7 +8,7 @@ contract testRequestString is ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
     address constant oracleSepolia = 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD;
-    string constant jobIdTaiko = "4a99df35ebe749aab98645ef6f03bf8f";
+    string constant jobIdSepolia = "7d80a6386ef543a3abb52817f6707e3b";
     uint256 public constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18 (0.1 LINK)
     string public currentPrice;
 
@@ -18,22 +18,22 @@ contract testRequestString is ChainlinkClient {
     );
 
     constructor() {
-        setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
+        _setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
     }
 
     function requestEthereumPrice() public {
-        Chainlink.Request memory req = buildChainlinkRequest(
-            stringToBytes32(jobIdTaiko),
+        Chainlink.Request memory req = _buildChainlinkRequest(
+            stringToBytes32(jobIdSepolia),
             address(this),
             this.fulfillEthereumPrice.selector
         );
-        req.add(
+        req._add(
             "get",
             "https://marcuswentz.github.io/chainlink_test_json_url_types/"
         );
-        req.add("path", "string");
+        req._add("path", "string");
         //req.addInt("times", 100);
-        sendChainlinkRequestTo(oracleSepolia, req, ORACLE_PAYMENT);
+        _sendChainlinkRequestTo(oracleSepolia, req, ORACLE_PAYMENT);
     }
 
     function fulfillEthereumPrice(
